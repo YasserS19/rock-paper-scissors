e913557363e7eb9369233233523b6c4c4b78c149
Yasser S <yasser.sissaoui@gmail.com>
2022-12-29 14:34:33 +0100

    added font

--- app.js
+++ app.js
@@ -8,14 +8,21 @@ while (play_again !== "n") {
     "Choose your hand: Rock, Paper or Scissors."
   ).toLowerCase();
   const cpuSelection = getComputerChoice();
+
+  if (!selections.includes(playerSelection)) {
+    console.log("Invalid move, moves are: Rock, Paper or Scissors.");
+    continue;
+  }
+  console.log(`Player choose: ${playerSelection}`);
+  console.log(`CPU choose: ${cpuSelection}`);
   playRound(playerSelection.toLowerCase(), cpuSelection, rounds);
 
   if (playerScore > cpuScore) {
-    alert("Player wins the game!");
+    console.log("Player wins the game!");
   } else if (playerScore < cpuScore) {
-    alert("CPU wins the game!");
+    console.log("CPU wins the game!");
   } else {
-    alert("The game is a draw!");
+    console.log("The game is a draw!");
   }
   play_again = prompt("Play again ? [y/n]:").toLowerCase();
 }
@@ -25,42 +32,30 @@ function getComputerChoice() {
 }
 function playRound(playerSelection, cpuSelection, rounds) {
   for (let _ = 0; _ < rounds; _++) {
-    alert(`Player choose: ${playerSelection}`);
-
-    if (!selections.includes(playerSelection)) {
-      alert("Invalid move, moves are: Rock, Paper or Scissors.");
-      playerSelection = prompt(
-        "Choose your hand again: Rock, Paper or Scissors."
-      );
-    } else {
-      alert(`CPU choose: ${cpuSelection}`);
+    if (playerSelection === cpuSelection) {
+      console.log("This round a draw!");
     }
-
-    if (playerSelection == cpuSelection) {
-      alert("This round a draw!");
-    }
-
     if (playerSelection == "paper" && cpuSelection == "rock") {
-      alert("Player wins this round!");
+      console.log("Player wins this round!");
       playerScore += 1;
     } else if (playerSelection == "rock" && cpuSelection == "paper") {
-      alert("CPU wins this round!");
+      console.log("CPU wins this round!");
       cpuScore += 1;
     }
 
     if (playerSelection == "rock" && cpuSelection == "scissors") {
-      alert("Player wins this round!");
+      console.log("Player wins this round!");
       playerScore += 1;
     } else if (playerSelection == "scissors" && cpuSelection == "rock") {
-      alert("CPU wins this round!");
+      console.log("CPU wins this round!");
       cpuScore += 1;
     }
 
     if (playerSelection == "scissors" && cpuSelection == "paper") {
-      alert("Player wins this round!");
+      console.log("Player wins this round!");
       playerScore += 1;
     } else if (playerSelection == "paper" && cpuSelection == "scissors") {
-      alert("CPU wins this round!");
+      console.log("CPU wins this round!");
       cpuScore += 1;
     }
   }
